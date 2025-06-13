d5e94cf9f8feb428232dfc150b0d4180621897ea
mmaciejek <chacinskimaciej@gmail.com>
2025-06-13 17:04:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wizualizacja/',
+  base: process.env.NODE_ENV === 'production' ? '/wizualizacja/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
